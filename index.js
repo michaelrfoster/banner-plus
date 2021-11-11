5cb1b564a332424e0654ab1b5b66c1719277c617
Mike <michaelrfoster0@gmail.com>
2021-11-11 11:02:06 -0500

    Moved input into its own div

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-let page = document.getElementById("buttonDiv");
+let buttonDiv = document.getElementById("buttonDiv");
+let inputDiv = document.getElementById("inputDiv");
 
 function handleButtonClick(event) {
 
   let br1 = document.createElement("br");
   let br2 = document.createElement("br");
-  page.appendChild(br1);
-  page.appendChild(br2);
+  inputDiv.appendChild(br1);
+  inputDiv.appendChild(br2);
 
-  let button = document.createElement("input");
-  button.setAttribute('class', 'input');
-  //button.textContent = 'NEW BUTTON';
-
-  //button.addEventListener("click", handleButtonClick);
-
-
-  page.appendChild(button);
+  let input = document.createElement("input");
+  input.setAttribute('class', 'input');
+  inputDiv.appendChild(input);
 }
 
 function constructPage() {
+  let input = document.createElement("input");
+  input.setAttribute('class', 'input');
+  inputDiv.appendChild(input);
+
   let button = document.createElement("button");
   button.setAttribute('class', 'btn');
-  button.textContent = 'test value';
+  button.textContent = 'Add more CRNs';
 
   button.addEventListener("click", handleButtonClick);
-  page.appendChild(button);
+  buttonDiv.appendChild(button);
 }
 
 constructPage()
