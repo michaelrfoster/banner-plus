d279a77cb7aebebf12ea8b866928ec22f1cccb4d
Mike <michaelrfoster0@gmail.com>
2021-11-11 20:37:03 -0500

    Reformatted spacing

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 let buttonDiv = document.getElementById("buttonDiv");
 let inputDiv = document.getElementById("inputDiv");
 
-function handleButtonClick(event) {
-  let br1 = document.createElement("br");
-  let br2 = document.createElement("br");
-  inputDiv.appendChild(br1);
-  inputDiv.appendChild(br2);
+function handleAddCRNButtonClick(event) {
 
   let input = document.createElement("input");
   input.setAttribute('class', 'input');
   inputDiv.appendChild(input);
+  insertBreaks(inputDiv, 2)
 }
 
 function constructPage() {
+
   let input = document.createElement("input");
   input.setAttribute('class', 'input');
   inputDiv.appendChild(input);
+  insertBreaks(inputDiv, 2)
 
-  let button = document.createElement("button");
-  button.setAttribute('class', 'btn');
-  button.textContent = 'Add more CRNs';
+  let addCRNButton = document.createElement("button");
+  addCRNButton.setAttribute('class', 'btn');
+  addCRNButton.textContent = 'Add more CRNs';
+  addCRNButton.addEventListener("click", handleAddCRNButtonClick);
+  buttonDiv.appendChild(addCRNButton);
 
-  button.addEventListener("click", handleButtonClick);
-  buttonDiv.appendChild(button);
+  let saveButton = document.createElement("button");
+  saveButton.setAttribute('class', 'btn');
+  saveButton.textContent = 'Save';
+  saveButton.addEventListener("click", handleSaveButtonClick);
+  buttonDiv.appendChild(saveButton);
+}
+
+function insertBreaks(div, numBreaks) {
+  for (i = 0; i < numBreaks; i++) {
+    let br1 = document.createElement("br");
+    div.appendChild(br1);
+  }
 }
 
 constructPage()
