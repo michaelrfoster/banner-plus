18a13914653112e788e020e3accf3b7ebd5e0eff
Mike <michaelrfoster0@gmail.com>
2021-11-15 14:41:05 -0500

    Test section now works with updated extension

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 let buttonDiv = document.getElementById("buttonDiv");
 let inputDiv = document.getElementById("inputDiv");
+let numInputs = 0;
 
 function handleAddCRNButtonClick(event) {
 
   let input = document.createElement("input");
   input.setAttribute('class', 'input');
+  input.setAttribute('id', 'txt_crn' + (numInputs+1) )
   inputDiv.appendChild(input);
   insertBreaks(inputDiv, 2)
+  numInputs++;
 }
 
 function constructPage() {
 
   let input = document.createElement("input");
   input.setAttribute('class', 'input');
+  input.setAttribute('id', 'txt_crn' + (numInputs+1) )
   inputDiv.appendChild(input);
   insertBreaks(inputDiv, 2)
+  numInputs++;
 
   let addCRNButton = document.createElement("button");
   addCRNButton.setAttribute('class', 'btn');
+  addCRNButton.setAttribute('id', 'addAnotherCRN' )
   addCRNButton.textContent = 'Add more CRNs';
   addCRNButton.addEventListener("click", handleAddCRNButtonClick);
   buttonDiv.appendChild(addCRNButton);
-
-  let saveButton = document.createElement("button");
-  saveButton.setAttribute('class', 'btn');
-  saveButton.textContent = 'Save';
-  saveButton.addEventListener("click", handleSaveButtonClick);
-  buttonDiv.appendChild(saveButton);
 }
 
 function insertBreaks(div, numBreaks) {
