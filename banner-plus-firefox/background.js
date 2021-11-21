a6259af2a06cd95df34565e206e0ba4844bedd34
Mike <michaelrfoster0@gmail.com>
2021-11-20 20:08:04 -0500

    Made final changes to firefox v1.0

--- banner-plus-firefox/background.js
+++ banner-plus-firefox/background.js
@@ -1,39 +1,35 @@
 const CRNs = [];
 var preferences = {};
 
+// Runs when initially installed
 chrome.runtime.onInstalled.addListener(() => {
   preferences["switchToEnterCRNsTabCheckBox"] = false;
   preferences["addToSummaryCheckBox"] = false;
   preferences["submitCheckBox"] = false;
   preferences["conditionalAddAndDropCheckBox"] = false;
 
-  //console.log("preferences", preferences)
-
   chrome.storage.sync.set({ preferences });
   chrome.storage.sync.set({ CRNs });
   console.log('Quick Banner is Active');
-  //console.log(preferences["Add to Summary"])
 });
 
-/*
+// Create the options selection in the context menu for the B+ icon
+browser.contextMenus.create({
+  "title": "Options",
+  "contexts": ["browser_action"],
+  "onclick": openOptionsPage
+});
 
-*/
+function openOptionsPage() {
+  var openingPage = browser.runtime.openOptionsPage()
+}
 
+// When the B+ icon is clicked, try inserting the CRNs
 browser.browserAction.onClicked.addListener((tab) => {
-  //const executing =
   chrome.tabs.executeScript(
     tab.id, {
       file: "insertCRNs.js"
   });
   executing.then(onExecuted, onError);
 });
-/*
-// background.js
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id},
-    files: ['insertCRNs.js']
-  });
-});
 
-*/
